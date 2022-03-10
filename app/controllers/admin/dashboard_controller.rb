85e3c83d5d392cad9c8959aa46fa37f4da2761f1
mikiyoshi <mikiyoshi.kokura@gmail.com>
2022-03-10 18:42:55 +0000

    Update dashboard page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
   def show
+    # .count is total number
+    @productcount = Product.count
+    @categorycount = Category.count
   end
 end
